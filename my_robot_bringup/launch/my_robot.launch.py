2986185142710d1f419223e2b7afd154db9f07ec
MarkusNguyen <11525061@student.vgu.edu.vn>
2026-06-06 20:32:50 +0700

    update

---
+++ my_robot_bringup/launch/my_robot.launch.py
@@ -0,0 +1,51 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import xacro
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    my_robot_controller_path = os.path.join(get_package_share_directory('my_robot_bringup'),
+                                'config', 'my_robot_controllers.yaml')
+    
+    robot_description_path = os.path.join(get_package_share_directory('my_robot_description'), 
+                                'mjcf', 'my_robot.urdf.xacro')
+    
+    robot_description = {'robot_description': xacro.process_file(robot_description_path).toxml()}
+
+    mujoco_simulator_node = Node(
+        package='mujoco_ros2_control',
+        executable='ros2_control_node',
+        output='screen',
+        parameters=[
+            robot_description,
+            my_robot_controller_path
+        ]   
+    )
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description]
+    )
+
+    joint_state_broadcaster_node = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=['joint_state_broadcaster']
+    )
+
+    ackermann_steering_controller_node = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=['ackermann_steering_controller']
+    )
+
+    return LaunchDescription([
+        mujoco_simulator_node,
+        robot_state_publisher_node,
+        joint_state_broadcaster_node,
+        ackermann_steering_controller_node
+    ])
--- my_robot_bringup/setup.py
+++ my_robot_bringup/setup.py
@@ -11,7 +11,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/config', glob('config/*'))
+        ('share/' + package_name + '/config', glob('config/*')),
+        ('share/' + package_name + '/launch', glob('launch/*'))
     ],
     package_data={'': ['py.typed']},
     install_requires=['setuptools'],
